bbe5e30602244814595ba6c521a7bf6b88b5a2fa
Savannah Ostrowski <savannahostrowski@gmail.com>
2023-04-02 13:53:38 -0700

    fix alembic;

--- api/database.py
+++ api/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 POSTGRES_DATABASE_URL=os.environ["DATABASE_URL"]
 
